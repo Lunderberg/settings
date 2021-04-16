eb290ab049f4d49d380f14e6fd8056a93bbf2305
Eric Lunderberg <elunderberg@octoml.ai>
2021-04-16 08:51:36 -0700

    Added clang-format on save to emacs, minor .gitconfig updates

--- ipython_startup/00-package_load.py
+++ ipython_startup/00-package_load.py
@@ -1,8 +1,5 @@
-import inspect
-import importlib
-
 class ModuleLoader:
-    def __init__(self, libname, import_as = None):
+    def __init__(self, libname, import_as=None):
         self.libname = libname
         if import_as is None:
             self.import_as = libname
@@ -10,12 +7,21 @@ class ModuleLoader:
             self.import_as = import_as
 
     def __getattr__(self, name):
+        import inspect, importlib
+
         module = importlib.import_module(self.libname)
         frame = inspect.stack()[1][0]
         frame.f_locals[self.import_as] = module
         return getattr(module, name)
 
-np = ModuleLoader('numpy','np')
-plt = ModuleLoader('matplotlib.pyplot','plt')
+
+# General data analysis
+np = ModuleLoader("numpy", "np")
+pd = ModuleLoader("pandas", "pd")
+plt = ModuleLoader("matplotlib.pyplot", "plt")
+
+# tvm import
+tvm = ModuleLoader("tvm", "tvm")
+te = ModuleLoader("tvm.te", "te")
 
 del ModuleLoader
