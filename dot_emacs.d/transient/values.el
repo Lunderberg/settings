c93a86366138d08dc4eab0909b810e46503584af
Eric Lunderberg <elunderberg@octoml.ai>
2022-01-12 09:44:11 -0600

    Preferred rebase settings for magit.

--- dot_emacs.d/transient/values.el
+++ dot_emacs.d/transient/values.el
@@ -1,1 +1,2 @@
-((magit-log:magit-log-mode "-n256" "--topo-order" "--graph" "--color" "--decorate"))
+((magit-log:magit-log-mode "-n256" "--topo-order" "--graph" "--color" "--decorate")
+ (magit-rebase "--autostash" "--interactive" "--rebase-merges=rebase-cousins"))
