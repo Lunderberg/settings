cbf4d094a3b9d36fc334f4d2d510468a8ad7d1d4
Eric Lunderberg <elunderberg@octoml.ai>
2023-06-12 14:31:56 -0500

    Added debug instrument, VerifyWellFormed

--- pylib/lunderberg_tvm_instrument.py
+++ pylib/lunderberg_tvm_instrument.py
@@ -153,3 +153,22 @@ class PrintTransformSequence:
         indent = self._indent()
         text = "\n".join(indent + line for line in text.split("\n"))
         print(text, flush=True)
+
+
+@pass_instrument
+class VerifyWellFormed:
+    def run_before_pass(self, mod, info):
+        if not tvm.tir.analysis.verify_well_formed(mod, assert_mode=False):
+            print("-*" * 30 + "-")
+            print(f"Failure prior to running {info.name}")
+            print(mod)
+            print("-*" * 30 + "-", flush=True)
+            tvm.tir.analysis.verify_well_formed(mod)
+
+    def run_after_pass(self, mod, info):
+        if not tvm.tir.analysis.verify_well_formed(mod, assert_mode=False):
+            print("-*" * 30 + "-")
+            print(f"Failure after running {info.name}")
+            print(mod)
+            print("-*" * 30 + "-", flush=True)
+            tvm.tir.analysis.verify_well_formed(mod)
