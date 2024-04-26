8b3d49c7a1c2eb58e65d1e7f1a7ed5301ffdad0c
Eric Lunderberg <elunderberg@octoml.ai>
2024-04-26 19:45:19 +0000

    Improvements to Timer utility

--- pylib/time_utils.py
+++ pylib/time_utils.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 import datetime
+import functools
+import logging
 import re
 import sys
 import termios
 import time
+from typing import Optional
+
+try:
+    import nvtx
+except ImportError:
+    logging.warn("NVTX library not found, will not be used")
+    nvtx = None
 
 
 def format_timedelta(d):
@@ -83,52 +92,115 @@ def get_clear_length(message_length):
 
 
 class Timer:
-    def __init__(self, message=None):
+    def __init__(
+        self,
+        message: Optional[str] = None,
+        print_to_stdout: bool = True,
+        log_to_nvtx: bool = True,
+    ):
+        """Time a region of python
+
+        Parameters
+        ----------
+        message: Optional[str]
+
+            The message to be printed.  If no message is provided,
+            then both `print_to_stdout` and `log_to_nvtx` have no
+            effect.
+
+        print_to_stdout: bool
+
+            If true, print when entering/exiting the context.  If
+            false, do not print to stdout.
+
+        log_to_nvtx: bool
+
+            If true, log when entering/exiting the context with NVTX.
+            If false, do not log.
+
+        """
+
         self.message = message
+        self.print_to_stdout = print_to_stdout
+        self.log_to_nvtx = log_to_nvtx
 
         self.start_datetime = None
         self.end_datetime = None
-        self.start_time = None
-        self.end_time = None
+        self.start_time_ns = None
+        self.end_time_ns = None
 
         self.is_running = False
+        self._nvtx_context = None
 
     def __enter__(self):
         self.start()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop()
+        self.stop(exc_type, exc_val, exc_tb)
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def inner(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+
+        return inner
 
     def start(self):
         if self.is_running:
             return
-
         self.is_running = True
-        self.start_datetime = datetime.datetime.now()
-        self.start_time = time.perf_counter()
 
-        if self.message is not None:
+        self.start_datetime = datetime.datetime.now()
+        self.start_time_ns = time.perf_counter_ns()
+
+        if self.message and self.print_to_stdout:
             advance_line()
             print(
                 "[{}]\t{}".format(self.start_datetime, self.message), end="", flush=True
             )
 
-    def stop(self):
-        if not self.is_running:
-            return
+        if self.message and self.log_to_nvtx and nvtx:
+            self._nvtx_context = nvtx.annotate(self.message)
+            self._nvtx_context.__enter__()
 
         self.is_running = False
+
+    def stop(self, exc_type=None, exc_val=None, exc_tb=None):
+        if not self.is_running:
+            return
+        self.is_running = False
+
         self.end_datetime = datetime.datetime.now()
-        self.end_time = time.perf_counter()
+        self.end_time_ns = time.perf_counter_ns()
 
-        self.elapsed_time = datetime.timedelta(seconds=self.end_time - self.start_time)
-
-        if self.message is not None:
+        if self.message and self.print_to_stdout:
             clear_len = get_clear_length(len(self.message))
             print(
                 "\r{}\r[{}]\t{}".format(
-                    " " * clear_len, format_timedelta(self.elapsed_time), self.message
+                    " " * clear_len, format_timedelta(self.elapsed), self.message
                 ),
                 flush=True,
             )
+
+        if self._nvtx_context:
+            nvtx_context = self._nvtx_context
+            self._nvtx_context = None
+            nvtx_context.__exit__(exc_type, exc_val, exc_tb)
+
+    @property
+    def elapsed(self) -> Optional[datetime.timedelta]:
+        if self.start_time_ns is None or self.end_time_ns is None:
+            return None
+        else:
+            return datetime.timedelta(
+                microseconds=(self.end_time_ns - self.start_time_ns) / 1000
+            )
+
+    @property
+    def elapsed_ns(self) -> Optional[int]:
+        if self.start_time_ns is None or self.end_time_ns is None:
+            return None
+        else:
+            return self.end_time_ns - self.start_time_ns
