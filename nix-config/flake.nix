423917935a629b3f373e384be1d668ab18891190
Eldritch Cheese <eldritch.cheese@gmail.com>
2025-12-15 20:38:44 -0600

    Clean up indentation of flake.nix

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -2,28 +2,28 @@
   description = "Flake config";
 
   inputs = {
-     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-     home-manager.url = "github:nix-community/home-manager/release-25.11";
-     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { self, nixpkgs, home-manager, ... }@inputs:
     {
-        nixosConfigurations.shelley = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                ./hosts/shelley.nix
-                ./modules/common.nix
-                ./modules/packages.nix
-                home-manager.nixosModules.home-manager
-                {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.eric = ./modules/home.nix;
-                }
-            ] ++ nixpkgs.lib.optional (builtins.pathExists ./secrets/localization.nix) ./secrets/localization.nix
-            ;
-        };
+      nixosConfigurations.shelley = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/shelley.nix
+          ./modules/common.nix
+          ./modules/packages.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.eric = ./modules/home.nix;
+          }
+        ] ++ nixpkgs.lib.optional (builtins.pathExists ./secrets/localization.nix) ./secrets/localization.nix
+        ;
+      };
     };
 }
